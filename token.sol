ec9160f5a7c480cf432947c53cd5a3936ad99751
zmnfl <1870504@gmail.com>
2024-09-23 11:41:29 +0300

    init

---
+++ token.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, ERC20Burnable, Ownable {
+    constructor(address initialOwner)
+        ERC20("Bixplorer.com", "BXE")
+        Ownable(initialOwner)
+    {
+        _mint(initialOwner, 200_000_000_000 ether);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function withdraw(address token, address to) public onlyOwner {
+        if (token == address(0)) {
+            uint256 balance = address(this).balance;
+            require(balance > 0, "Insufficient balance");
+            payable(to).transfer(balance);
+        } else {
+            IERC20 erc20Token = IERC20(token);
+            uint256 balance = erc20Token.balanceOf(address(this));
+            require(balance > 0, "Insufficient balance");
+            erc20Token.transfer(to, balance);
+        }
+    }
+}
