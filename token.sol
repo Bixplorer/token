092ed502a37457e1f067f077294bd75acd431dbb
zmnfl <1870504@gmail.com>
2024-09-26 18:07:15 +0300

    fix for audit

--- token.sol
+++ token.sol
@@ -3,17 +3,19 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, ERC20Burnable, Ownable {
+contract MyToken is ERC20, ERC20Burnable {
+    address private owner;
+
     constructor(address initialOwner)
         ERC20("Bixplorer.com", "BXE")
-        Ownable(initialOwner)
     {
+        owner = initialOwner;
         _mint(initialOwner, 200_000_000_000 ether);
     }
 
-    function withdraw(address token, address to) public onlyOwner {
+    function withdraw(address token, address to) public {
+        require(msg.sender == owner, "You're not the owner"); 
         if (token == address(0)) {
             uint256 balance = address(this).balance;
             require(balance > 0, "Insufficient balance");
